81957530afe1befb0345ec2498bf5a8bd5bb9302
zairadifini <zairadifini123@gmail.com>
2025-09-25 20:25:40 -0300

    .

--- pryDiFiniLaMilanga/frmLaMilanga.Designer.cs
+++ pryDiFiniLaMilanga/frmLaMilanga.Designer.cs
@@ -33,6 +33,12 @@
             BebidasSinAlcohol = new DataGridViewTextBoxColumn();
             BebidasConAlcohol = new DataGridViewTextBoxColumn();
             Postres = new DataGridViewTextBoxColumn();
+            btnValidarDatos = new Button();
+            btnSalir = new Button();
+            btnMozoDelDia = new Button();
+            btnTotales = new Button();
+            lblResultadoMozoDelDia = new Label();
+            lblResultadoTotales = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvVentas).BeginInit();
             SuspendLayout();
             // 
@@ -43,7 +49,7 @@
             dgvVentas.Location = new Point(12, 21);
             dgvVentas.Name = "dgvVentas";
             dgvVentas.RowHeadersWidth = 51;
-            dgvVentas.Size = new Size(827, 188);
+            dgvVentas.Size = new Size(552, 188);
             dgvVentas.TabIndex = 0;
             // 
             // Comidas
@@ -51,49 +57,122 @@
             Comidas.HeaderText = "Comidas";
             Comidas.MinimumWidth = 6;
             Comidas.Name = "Comidas";
-            Comidas.Width = 200;
+            Comidas.Width = 125;
             // 
             // BebidasSinAlcohol
             // 
             BebidasSinAlcohol.HeaderText = "Bebidas sin alcohol";
             BebidasSinAlcohol.MinimumWidth = 6;
             BebidasSinAlcohol.Name = "BebidasSinAlcohol";
-            BebidasSinAlcohol.Width = 200;
+            BebidasSinAlcohol.Width = 125;
             // 
             // BebidasConAlcohol
             // 
             BebidasConAlcohol.HeaderText = "Bebidas con alcohol";
             BebidasConAlcohol.MinimumWidth = 6;
             BebidasConAlcohol.Name = "BebidasConAlcohol";
-            BebidasConAlcohol.Width = 200;
+            BebidasConAlcohol.Width = 125;
             // 
             // Postres
             // 
             Postres.HeaderText = "Postres";
             Postres.MinimumWidth = 6;
             Postres.Name = "Postres";
-            Postres.Width = 200;
+            Postres.Width = 125;
+            // 
+            // btnValidarDatos
+            // 
+            btnValidarDatos.Location = new Point(574, 170);
+            btnValidarDatos.Name = "btnValidarDatos";
+            btnValidarDatos.Size = new Size(141, 39);
+            btnValidarDatos.TabIndex = 1;
+            btnValidarDatos.Text = "Validar Datos";
+            btnValidarDatos.UseVisualStyleBackColor = true;
+            btnValidarDatos.Click += btnValidarDatos_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(441, 401);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(123, 31);
+            btnSalir.TabIndex = 2;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // btnMozoDelDia
+            // 
+            btnMozoDelDia.Enabled = false;
+            btnMozoDelDia.Location = new Point(12, 241);
+            btnMozoDelDia.Name = "btnMozoDelDia";
+            btnMozoDelDia.Size = new Size(141, 39);
+            btnMozoDelDia.TabIndex = 3;
+            btnMozoDelDia.Text = "Mozo del dia";
+            btnMozoDelDia.UseVisualStyleBackColor = true;
+            btnMozoDelDia.Click += btnMozoDelDia_Click;
+            // 
+            // btnTotales
+            // 
+            btnTotales.Enabled = false;
+            btnTotales.Location = new Point(12, 325);
+            btnTotales.Name = "btnTotales";
+            btnTotales.Size = new Size(141, 39);
+            btnTotales.TabIndex = 4;
+            btnTotales.Text = "Totales";
+            btnTotales.UseVisualStyleBackColor = true;
+            btnTotales.Click += btnTotales_Click;
+            // 
+            // lblResultadoMozoDelDia
+            // 
+            lblResultadoMozoDelDia.AutoSize = true;
+            lblResultadoMozoDelDia.Location = new Point(172, 250);
+            lblResultadoMozoDelDia.Name = "lblResultadoMozoDelDia";
+            lblResultadoMozoDelDia.Size = new Size(159, 20);
+            lblResultadoMozoDelDia.TabIndex = 5;
+            lblResultadoMozoDelDia.Text = "ResultadoMozoDelDia";
+            // 
+            // lblResultadoTotales
+            // 
+            lblResultadoTotales.AutoSize = true;
+            lblResultadoTotales.Location = new Point(172, 334);
+            lblResultadoTotales.Name = "lblResultadoTotales";
+            lblResultadoTotales.Size = new Size(122, 20);
+            lblResultadoTotales.TabIndex = 6;
+            lblResultadoTotales.Text = "ResultadoTotales";
             // 
             // frmLaMilanga
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(851, 381);
+            ClientSize = new Size(727, 468);
+            Controls.Add(lblResultadoTotales);
+            Controls.Add(lblResultadoMozoDelDia);
+            Controls.Add(btnTotales);
+            Controls.Add(btnMozoDelDia);
+            Controls.Add(btnSalir);
+            Controls.Add(btnValidarDatos);
             Controls.Add(dgvVentas);
             Name = "frmLaMilanga";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "La Milanga";
-            Load += this.frmLaMilanga_Load;
+            Load += frmLaMilanga_Load;
             ((System.ComponentModel.ISupportInitialize)dgvVentas).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvVentas;
+        private Button btnValidarDatos;
+        private Button btnSalir;
         private DataGridViewTextBoxColumn Comidas;
         private DataGridViewTextBoxColumn BebidasSinAlcohol;
         private DataGridViewTextBoxColumn BebidasConAlcohol;
         private DataGridViewTextBoxColumn Postres;
+        private Button btnMozoDelDia;
+        private Button btnTotales;
+        private Label lblResultadoMozoDelDia;
+        private Label lblResultadoTotales;
     }
 }
--- pryDiFiniLaMilanga/frmLaMilanga.cs
+++ pryDiFiniLaMilanga/frmLaMilanga.cs
@@ -6,5 +6,131 @@ namespace pryDiFiniLaMilanga
         {
             InitializeComponent();
         }
+
+        private void frmLaMilanga_Load(object sender, EventArgs e)
+        {
+            string[] mozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+
+            // Agregar filas vacías
+            foreach (string mozo in mozos)
+            {
+                dgvVentas.Rows.Add(0, 0, 0, 0); // 4 columnas inicializadas en 0
+            }
+
+            // Poner los nombres de los mozos en los encabezados de fila
+            for (int i = 0; i < mozos.Length; i++)
+            {
+                dgvVentas.Rows[i].HeaderCell.Value = mozos[i];
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnValidarDatos_Click(object sender, EventArgs e)
+        {
+            bool DatosValidos = true;
+
+            // Recorro filas
+            for (int i = 0; i < dgvVentas.Rows.Count; i++)
+            {
+                // Recorro columnas
+                for (int j = 0; j < dgvVentas.Columns.Count; j++)
+                {
+                    string valor = dgvVentas.Rows[i].Cells[j].Value?.ToString() ?? "0";
+
+                    // Intento convertir a número
+                    try
+                    {
+                        float num = Convert.ToSingle(valor);
+                    }
+                    catch
+                    {
+                        // Si da error, hay algo mal
+                        DatosValidos = false;
+                    }
+                }
+            }
+
+            if (DatosValidos)
+            {
+                MessageBox.Show("Todos los datos están correctos");
+                btnMozoDelDia.Enabled = true;
+                btnTotales.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Error: hay celdas que no son números");
+                btnMozoDelDia.Enabled = false;
+                btnTotales.Enabled = false;
+            }
+        }
+
+        private void btnMozoDelDia_Click(object sender, EventArgs e)
+        {
+            int MozoMayor = 0;
+            int ImporteMayor = 0;
+
+            for (int i = 0; i < dgvVentas.Rows.Count; i++)
+            {
+                // Ignorar fila de nueva entrada
+                if (dgvVentas.Rows[i].IsNewRow) continue;
+
+                int Acumulador = 0;
+
+                // Recorro columnas
+                for (int j = 0; j < dgvVentas.Columns.Count; j++)
+                {
+                    int valor = 0;
+
+                    // Convertir de forma segura
+                    if (dgvVentas.Rows[i].Cells[j].Value != null &&
+                        int.TryParse(dgvVentas.Rows[i].Cells[j].Value.ToString(), out int temp))
+                    {
+                        valor = temp;
+                    }
+
+                    Acumulador += valor;
+                }
+
+                // Ver cuál mozo tiene el mayor total
+                if (Acumulador > ImporteMayor)
+                {
+                    ImporteMayor = Acumulador;
+                    MozoMayor = i;
+                }
+            }
+            string[] mozos = { "Julio", "Esteban", "Javier", "Gonzalo", "Alberto" };
+
+            lblResultadoMozoDelDia.Text = "Mozo del día: " + mozos[MozoMayor] +
+                              " - Importe $: " + ImporteMayor.ToString();
+        }
+
+        private void btnTotales_Click(object sender, EventArgs e)
+        {
+            int totalComidas = 0;
+            int totalBebidasSin = 0;
+            int totalBebidasCon = 0;
+            int totalPostres = 0;
+            int totalGeneral = 0;
+
+            for (int i = 0; i < dgvVentas.Rows.Count; i++)
+            {
+                totalComidas += int.Parse(dgvVentas.Rows[i].Cells[0].Value?.ToString() ?? "0");
+                totalBebidasSin += int.Parse(dgvVentas.Rows[i].Cells[1].Value?.ToString() ?? "0");
+                totalBebidasCon += int.Parse(dgvVentas.Rows[i].Cells[2].Value?.ToString() ?? "0");
+                totalPostres += int.Parse(dgvVentas.Rows[i].Cells[3].Value?.ToString() ?? "0");
+            }
+
+            totalGeneral = totalComidas + totalBebidasSin + totalBebidasCon + totalPostres;
+
+            lblResultadoTotales.Text = "Total Comidas: " + totalComidas + "\n" +
+                                       "Total Bebidas sin alcohol: " + totalBebidasSin + "\n" +
+                                       "Total Bebidas con alcohol: " + totalBebidasCon + "\n" +
+                                       "Total Postres: " + totalPostres + "\n" +
+                                       "Total General: " + totalGeneral;
+        }
     }
 }
